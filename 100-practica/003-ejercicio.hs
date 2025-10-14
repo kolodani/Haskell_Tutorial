bdf51bdcb2b04fd8f9488ca64396266472b8a5fd
kolodani <danielvinzia@gmail.com>
2025-10-14 13:18:04 -0300

    Practica, 003

---
+++ 100-practica/003-ejercicio.hs
@@ -0,0 +1,17 @@
+{-
+3) Definir una función que determine si un año es bisiesto o no, de
+acuerdo a la siguiente definición:
+
+año bisiesto 1. m. El que tiene un día más que el año común, añadido al mes de febrero. Se repite
+cada cuatro años, a excepción del último de cada siglo cuyo número de centenas no sea múltiplo
+de cuatro. (Diccionario de la Real Academia Espaola, 22ª ed.)
+
+¿Cuál es el tipo de la función definida?
+-}
+
+bisiesto :: Int -> String
+bisiesto x
+  | mod x 400 == 0 = "Es bisiesto"
+  | mod x 100 == 0 = "No es bisiesto"
+  | mod x 4 == 0 = "Es bisiesto"
+  | otherwise = "No es bisiesto"
--- 100-practica/100-Practica.Laboratorio.1.hs
+++ 100-practica/100-Practica.Laboratorio.1.hs
@@ -3,34 +3,6 @@ module Practica0 where
 import Data.List
 
 {-
-2. Definir las siguientes funciones y determinar su tipo:
-
-a) five, que dado cualquier valor, devuelve 5
-
-b) apply, que toma una función y un valor, y devuelve el resultado de
-aplicar la función al valor dado
-
-c) identidad, la función identidad
-
-d) first, que toma un par ordenado, y devuelve su primera componente
-
-e) derive, que aproxima la derivada de una función dada en un punto dado
-
-f) sign, la función signo
-
-g) vabs, la función valor absoluto (usando sign y sin usarla)
-
-h) pot, que toma un entero y un número, y devuelve el resultado de
-elevar el segundo a la potencia dada por el primero
-
-i) xor, el operador de disyunción exclusiva
-
-j) max3, que toma tres números enteros y devuelve el máximo entre llos
-
-k) swap, que toma un par y devuelve el par con sus componentes invertidas
--}
-
-{-
 3) Definir una función que determine si un año es bisiesto o no, de
 acuerdo a la siguiente definición:
 
