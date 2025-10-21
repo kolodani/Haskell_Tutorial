3473354b239f12e1bf8d7203a2e84cfd03dce933
kolodani <danielvinzia@gmail.com>
2025-10-21 08:26:52 -0300

    Practica, 006

--- 100-practica/100-Practica.Laboratorio.1.hs
+++ 100-practica/100-Practica.Laboratorio.1.hs
@@ -3,26 +3,6 @@ module Practica0 where
 import Data.List
 
 {-
-5) Definir las siguientes funciones usando listas por comprensión:
-
-a) 'divisors', que dado un entero positivo 'x' devuelve la
-lista de los divisores de 'x' (o la lista vacía si el entero no es positivo)
-
-b) 'matches', que dados un entero 'x' y una lista de enteros descarta
-de la lista los elementos distintos a 'x'
-
-c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
-'(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
-donde 0 <= a, b, c, d <= 'n'
-
-(d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
-'xs' sin elementos repetidos
-unique :: [Int] -> [Int]
--}
-
-
-
-{-
 6) El producto escalar de dos listas de enteros de igual longitud
 es la suma de los productos de los elementos sucesivos (misma
 posición) de ambas listas.  Definir una función 'scalarProduct' que
